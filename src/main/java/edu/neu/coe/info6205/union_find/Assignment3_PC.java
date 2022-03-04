c7478c3887afc5003b152815a4adc5b5d2a7ee41
Shivani31996 <shivani.chavan96@gmail.com>
2022-03-04 11:33:30 -0500

    pushingCode

--- src/main/java/edu/neu/coe/info6205/union_find/Assignment3_PC.java
+++ src/main/java/edu/neu/coe/info6205/union_find/Assignment3_PC.java
@@ -1,7 +1,30 @@
 package edu.neu.coe.info6205.union_find;
 
-public class Assignment3_PC {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Assignment3_PC {
+
+        public static int count(int n){
+            UF_HWQUPC uf = new UF_HWQUPC(n);
+            Random random = new Random();
+            int cnt =0;
+
+            while(uf.components()!=1)
+            {
+                int p= random.nextInt(n);
+                int q= random.nextInt(n);
+                cnt++;
+                if(!uf.connected(p,q))
+                    uf.union(p,q);
+            }
+
+            return  cnt;
+        }
+
+        public static void main(String args[]){
+                Scanner scanner = new Scanner(System.in);
+                int n = scanner.nextInt();
+                System.out.println("Number of initial sites " + n + " number of pairs generated " + count(n));
+        }
     }
-}
